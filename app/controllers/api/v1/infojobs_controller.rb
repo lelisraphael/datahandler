044edea9e0bad023d193d69b01b1ea48224d4636
Raphael <raphael>
2023-04-03 00:18:10 -0300

    add infojobs service

--- app/controllers/api/v1/infojobs_controller.rb
+++ app/controllers/api/v1/infojobs_controller.rb
@@ -2,72 +2,98 @@ module Api
   module V1
     class InfojobsController < ApplicationController
       def index
-        candidates = Candidate.includes(:experiences).all
-        render json: candidates
+        # candidates = Candidate.includes(:experiences).all
+        # render json: candidates
       end
 
-      def create
-        text = params['Candidatos']['Bloco01']
-        # candidate = parse_candidate(text)
-        render json: text
+     def create
+  text = params['Candidatos']
+  candidates = []
+
+
+
+
+
+
+  text.map do  | candidate |
+
+  
+experiences = candidate['Bloco01'].split("Experiência profissional") if candidate
+
+academic = experiences[0]
+professional = experiences[1]
+
+
+regex =/(?:^Formação Acadêmica$|(?:\n|\r)[A-Z][a-z]{2}\. \d{4} até (?:[A-Z][a-z]{2}\. \d{4}|o momento(?: \(Finalização prevista - [A-Z][a-z]{2}\. \d{4}\))?))\r/
+
+# Academic
+
+academic_experience = []
+    academic.scan(regex).flatten.each do |periodo|
+      descricao = academic.split(periodo)[0].lines.last.strip
+      academic_experience << { periodo: periodo, descricao: descricao }
+    end
+
+    # Agrupa academic_experience por período, evitando descrições repetidas
+    agrupados = {}
+    academic_experience.each do |r|
+      periodo = r[:periodo].gsub(/o momento/, Time.now.strftime("%b. %Y"))
+      agrupados[periodo] ||= []
+      unless agrupados[periodo].any? {|a| a[:descricao] == r[:descricao] }
+        agrupados[periodo] << r
       end
+    end
+
+    # Professional
+    professional_experience = []
+    professional.scan(regex).flatten.each do |periodo|
+      descricao = professional.split(periodo)[0].lines.last.strip
+      professional_experience << { periodo: periodo, descricao: descricao }
+    end
+
+    # Agrupa professional_experience por período, evitando descrições repetidas
+    agrupados_prof = {}
+    professional_experience.each do |r|
+      periodo = r[:periodo].gsub(/o momento/, Time.now.strftime("%b. %Y"))
+      agrupados_prof[periodo] ||= []
+      unless agrupados_prof[periodo].any? {|a| a[:descricao] == r[:descricao] }
+        agrupados_prof[periodo] << r
+      end
+    end
+
+    candidate = {
+      Name: candidate["Nome"],
+      URL: candidate['URL'],
+      JobRole: "",
+      CompanyName: "",
+      Local: candidate['Localidade'],
+      Obs: '',
+      ExtraCode: candidate['URL'],
+      Email: candidate['Email'],
+      Phone: candidate['Telephone'],
+      DesiredSalary: '',
+      Area: "",
+      Contract: '',
+      WorkTime: '',
+      WorkModel: '',
+      Level: "",
+      Source: 'Infojobs',
+      AcademicExperience: agrupados.values.flatten,
+      ProfessionalExperience: agrupados_prof.values.flatten,
+      # academic: array[0],
+      # professional: array[1],
+      
+    }
+
+    candidates <<  candidate
+  end
+
+  render json: candidates
+end
 
       private
+      
 
-      def parse_candidate(text)
-        candidate = {}
-        candidate[:AcademicExperience] = []
-        candidate[:ProfessionalExperience] = []
-
-        # Regex para encontrar informações relevantes do currículo
-        name = text.match(/Nome:(.*)/) && $1.strip
-        email = text.match(/Email:(.*)/) && $1.strip
-        phone = text.match(/Telefone:(.*)/) && $1.strip
-        address = text.match(/Endereço:(.*)/) && $1.strip
-        academic_experience = text.match(/Formação Acadêmica:(.*?)Experiência Profissional:/m) && $1.strip
-        professional_experience = text.match(/Experiência Profissional:(.*)/m) && $1.strip
-
-        # Preenchendo as informações do candidato no objeto JSON
-        candidate[:Name] = name if name.present?
-        candidate[:Email] = email if email.present?
-        candidate[:Phone] = phone if phone.present?
-        candidate[:Address] = address if address.present?
-
-        # Preenchendo as informações de formação acadêmica no objeto JSON
-        if academic_experience.present?
-          academic_experience.scan(/(.*)- (.*) - (.*) - (.*)/) do |course, level, company, city|
-            candidate[:AcademicExperience] << {
-              Course: course.strip,
-              Level: level.strip,
-              Company: company.strip,
-              City: city.strip,
-              StartDate: "",
-              EndDate: "",
-              Type: "Academic"
-            }
-          end
-        end
-
-        # Preenchendo as informações de experiência profissional no objeto JSON
-        if professional_experience.present?
-          professional_experience.scan(/(.*) - (.*) - (.*) - (.*) - (.*)/) do |job_role, level, company, city, date_range|
-            start_date, end_date = date_range.split("-").map(&:strip)
-            candidate[:ProfessionalExperience] << {
-              Salary: "",
-              Area: "",
-              Level: level.strip,
-              JobRole: job_role.strip,
-              Company: company.strip,
-              StartDate: start_date,
-              EndDate: end_date,
-              Type: "Work",
-              Description: ""
-            }
-          end
-        end
-
-        candidate
-      end
     end
   end
 end
